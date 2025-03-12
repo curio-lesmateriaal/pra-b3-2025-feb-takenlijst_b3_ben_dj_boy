c5960c3a35aaa034c3bb5fa8d14b0be8bac56ab2
Ben <D310581@edu.curio.nl>
2025-03-12 14:37:46 +0100

    begin

--- index.php
+++ index.php
@@ -11,6 +11,40 @@
     <div class="container">
 
 
+    <?php foreach ($tasks as $task): ?>
+    <tr>
+        <td><?= $task['title'] ?></td>
+        <td><?= $task['description'] ?></td>
+        <td><?= $task['department'] ?></td>
+        <td><?= $task['status'] ?></td>
+        <td>
+            <a href="?delete=<?= $task['id'] ?>">Verwijderen</a>
+            <form method="POST" style="display:inline;">
+                <input te="type="hidden"  name="id" value="<?= $task['id'] ?>">
+                <input typext" name="title" value="<?= $task['title'] ?>">
+                <input type="text" name="description" value="<?= $task['description'] ?>">
+                <select name="department">
+                    <option value="personeel">Personeel</option>
+                    <              <option value="techniek">Techniek</option>
+                    <option value="inkoop">Inkoop</option>
+                    <option value="klantenservice">Klantenservice</option>
+                    <option value="groen">Groen</ooption value="horeca">Horeca</option>
+                </select>
+                <select name="status">
+                    <option value="todo">Todo</option>
+                    <option value="done">Done</option>
+                </select>
+                <button type="submit" name="update_task">Bewerken</button>
+            </form>
+        </td>
+    </tr>
+    <?php endforeach; ?>
+</table>
+
+</body>
+</html>
+
+
     </div>
 
 </body>
