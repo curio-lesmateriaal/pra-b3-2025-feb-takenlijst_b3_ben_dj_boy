d982fd77693876c742a1ccfd538da9e9734cf026
TemdoesSpeedruns <djpangoro@gmail.com>
2025-03-20 09:37:11 +0100

    Update header.php

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
                     <a href="/index.php"><button>Home</button></a>
                     <a href="/task/index.php"><button>Taken Lijst</button></a>
                     <a href="/task/create.php"><button>Nieuwe Taak</button></a>
+                    <a href="/task/edit.php"><button>Bewerk Taak</button></a>
 
                 </nav>
             </div>
