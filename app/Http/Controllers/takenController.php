84f94a52faed41b8cafabf786202bbc506eb6fc9
Ben <D310581@edu.curio.nl>
2025-03-20 09:19:45 +0100

    user veranderd

--- app/Http/Controllers/takenController.php
+++ app/Http/Controllers/takenController.php
@@ -50,82 +50,30 @@ if (isset($_POST['action'])) {
 <?php
 
 if($action == "edit"){
+    
 
-//Variabelen vullen
-$attractie = $_POST['attractie'];
-$capaciteit = $_POST['capaciteit'];
-$melder = $_POST['melder'];
-$overige_info = $_POST['overige_info'];
-$id = $_POST['id'];
+} 
 
 
-
-// if(empty($overige_info) or (empty($attractie)) or (empty($type)) or (empty($capaciteit)) or (empty($melder)));
-// {
-// $errors[] = "vul alle velden in.";
-// }
-
-
-if(isset($_POST['prioriteit']))
-{
-$prioriteit = 1;
-}
-else
-{
-$prioriteit = 0;
-}
-
-
-
-// if(isset($errors)){
-// print_r($errors);
-// die();
-// }
-
-
-//1. Verbinding
-require_once '../../../config/conn.php';
-
-//2. Query
-$query = "UPDATE meldingen SET attractie = :attractie, capaciteit = :capaciteit,
-melder = :melder, prioriteit = :prioriteit, overige_info = :overige_info
-WHERE id = :id";
-
-//3. Prepare
-$statement = $conn->prepare($query);
-
-//4. Execute
-$statement->execute([
-":attractie" => $attractie,
-":capaciteit" => $capaciteit,
-":melder" => $melder,
-":prioriteit" => $prioriteit,
-":overige_info" => $overige_info,
-":id" => $id
-]);
-
-header("Location: ../../../resources/views/meldingen/index.php?msg=Melding is aangepast!");
-}
 if($action == "delete") {
 
-//variabelen vullen
-$id = $_POST['id'];
+    //variabelen vullen
+    $id = $_POST['id'];
 
-//1. verbinding
-require_once '../../../config/conn.php';
+    //1. verbinding
+    require_once '../../../backend/conn.php';
 
-//2. query
-$query = "DELETE FROM meldingen WHERE id = :id";
+    //2. query
+    $query = "DELETE FROM meldingen WHERE id = :id";
 
-//3. prepare
-$statement = $conn->prepare($query);
+    //3. prepare
+    $statement = $conn->prepare($query);
 
-//4. Execute
-$statement->execute([
-":id" => $id
-]);
+    //4. Execute
+    $statement->execute([
+    ":id" => $id
+    ]);
 
-header("Location: ../../../resources/views/meldingen/index.php?msg=Melding is verwijderd!");
+    header("Location: ../../../index.php?msg=Melding is verwijderd!");
 }
-
 ?>
