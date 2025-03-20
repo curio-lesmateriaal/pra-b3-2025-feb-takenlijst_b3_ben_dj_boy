aea4a3f74cbd10fa6fd9618938039fc37d02c260
BoyBrouwers999 <boybrouwers2007@hotmail.com>
2025-03-20 09:45:24 +0100

    Boy Edit
    create.php in edit.php

--- app/Http/Controllers/takenController.php
+++ app/Http/Controllers/takenController.php
@@ -49,13 +49,46 @@ if (isset($_POST['action'])) {
 
 <?php
 
-if($action == "edit"){
-    
+if ($action == "edit") {
 
-} 
+    // Variabelen vullen
+    $titel = $_POST['titel'];
+    $beschrijving = $_POST['beschrijving'];
+    $afdeling = $_POST['afdeling'];
+    $deadline = $_POST['deadline'];
+    $status = $_POST['status'];
+    $user = $_POST['user'];
+    $created_at = $_POST['created_at'];
+
+    // 1. Verbinding
+    require_once '../../../backend/conn.php';
+
+    // 2. Query
+    $query = "UPDATE taken (titel, beschrijving, afdeling, deadline, status, user, created_at) 
+                  VALUES(:titel, :beschrijving, :afdeling, :deadline, :status, :user, :created_at)";
+
+    // 3. Prepare
+    $statement = $conn->prepare($query);
+
+    // 4. Execute
+    if ($statement->execute([
+        ":titel" => $titel,
+        ":beschrijving" => $beschrijving,
+        ":afdeling" => $afdeling,
+        ":deadline" => $deadline,
+        ":status" => $status,
+        ":created_at" => $created_at,
+        ":user" => $user
+    ])) {
+        header("Location: ../../../index.php?msg=Melding opgeslagen");
+        exit;  // Zorg ervoor dat na de header geen verdere code wordt uitgevoerd
+    } else {
+        echo "Er is een fout opgetreden tijdens het opslaan.";
+    }
+}
 
 
-if($action == "delete") {
+if ($action == "delete") {
 
     //variabelen vullen
     $id = $_POST['id'];
@@ -71,7 +104,7 @@ if($action == "delete") {
 
     //4. Execute
     $statement->execute([
-    ":id" => $id
+        ":id" => $id
     ]);
 
     header("Location: ../../../index.php?msg=Melding is verwijderd!");
