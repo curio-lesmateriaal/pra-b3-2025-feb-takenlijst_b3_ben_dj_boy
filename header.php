79f44d6961f395c924f9b49fa2bbbaa25e0b199c
TemdoesSpeedruns <djpangoro@gmail.com>
2025-04-10 11:45:36 +0200

    Inloggen knop werkt
    het brengt je nog niet terug naar home dus inloggen werkt nog niet helemaal., de wachtwoord knop moet ook nog groter

--- header.php
+++ header.php
@@ -7,12 +7,19 @@
             </div>
             <div class="buttonsNav">
                 <nav>
-
+                <?php if (isset($_SESSION['user_id'])): ?>
+                    <a href="<?php echo $base_url; ?>/logout.php"><button>Uitloggen</button></a>
+            <?php else: ?>
+                <a href="/login/login.php"><button>Inloggen</button></a>
+            <?php endif; ?>
                     <a href="/index.php"><button>Home</button></a>
                     <a href="/task/index.php"><button>Taken Lijst</button></a>
                     <a href="/task/create.php"><button>Nieuwe Taak</button></a>
                     <a href="/task/done.php"><button>Voltooide taken</button></a>
                 </nav>
             </div>
+            <header>
+
+
 
         </header>
---
+++ login/login.php
@@ -0,0 +1,41 @@
+<?php session_start(); ?>
+
+<!doctype html>
+<html lang="nl">
+
+<head>
+    <title>takenController</title>
+    <?php require_once '../head.php'; ?>
+</head>
+
+<body>
+
+<?php require_once '../header.php'; ?>
+
+    
+    <div class="container">
+
+        <h1>takenController/ Inloggen</h1>
+        <?php
+        if(isset($_GET['msg']))
+        {
+            echo "<div class='msg'>" . $_GET['msg'] . "</div>";
+        }
+        ?>
+
+        <form action="backend/loginController.php" method="POST">
+            <div class="form-group">
+                <label for="username">Gebruikersnaam:</label>
+                <input type="text" name="username" id="username" placeholder="user1">
+            </div>
+            <div class="form-group">
+                <label for="password">Wachtwoord:</label>
+                <input type="password" name="password" id="password" placeholder="pass">
+            </div>
+            <input type="submit" value="Login">
+        </form>
+    </div>
+
+</body>
+
+</html>
